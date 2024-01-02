b4ab00b02014c718582312f42eb159dc1ee9ed5e
Hoang Minh Tri <minhtri99hn@gmail.com>
2024-01-02 22:28:11 +0700

    add class word model

---
+++ Tris/extract_engine/WordModel.py
@@ -0,0 +1,15 @@
+"""
+	Define WordModel 
+"""
+
+class WordMode:
+	def __init__(self,word:str,spelling:str,definition:str,vietnam_def:str,example:str,level:str):
+		self.word = word
+		self.spelling = spelling
+		self.definition = definition
+		self.vietnam_def  = vietnam_def
+		self.example  = example
+		self.level = level
+	def __str__(self):
+		return f"""The word: {self.word} with the information : {self.spelling},
+		{self.definition}, {self.vietnam_def}, {self.example}, {self.level}"""
