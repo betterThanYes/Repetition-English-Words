b9f36e2d08e0f2581afc4bde3aaba98ceeb064e2
ngochuy2910 <ngochuy.vardy99@gmail.com>
2024-01-09 00:43:54 +0700

    Commit to request chatGPT

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #from extract_engine.WordController import WordController
+from extract_engine.WordModel import WordModel
 if __name__ == "__main__":
     
         #list_word = extract_list_words_from_line()
@@ -11,8 +12,14 @@ if __name__ == "__main__":
     try:
         with open("extract_engine/result_after_extract.txt",'r',encoding='utf-8') as outfile:
             list_words = outfile.read().split("\n")
+            list_word_after = []
             for word in list_words:
                 print(word)
+                word, spelling , definition, example, level = requestAPIWord(word)
+                wordModel = WordModel(word, spelling , definition, example, level)
+                list_word_after.append(wordModel)
+
+
             
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
